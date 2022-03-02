92d0adc95f88126b0c8cc93ce714f80514f6f168
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-03-01 19:17:13 -0500

    added server/server.js

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Book-Search-Engine-v1
+# BKSB Book Search Engine v1
+
+The BKSB Book Search Engine is a web application that allows avid readers to search for books, save them to their profile, and revise their collection at well. This collection can be an online collection of favorite books or a wishlist of books. 
+
+## Features
+
+Every collection is saved to their user's account and can be accessed by the user every time they log in. New users will be able to create an account and start their book collections as well.
+
+![It should look like this](loadscreen.png)
+
+Users will be able to use a search bar to find books based on their queries. They can then pick from the search results and add it to their collection. At any time, they can view and edit their own collection.
+
+![It should look like this](loadscreen.png)
+## Deployment
+
+Because this is a web application, the entirety of the application can be accessed online on [Heroku](www.google.com) and does not need to be installed. 
+
+## Contributions
+
+This project is closed for contributions and is meant to be used for educational purposes only. However, if you would like to develop the app individually, the repository containing the code can be found [here](https://github.com/jhahnsheen/Book-Search-Engine-v1).
--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// import the graphql schema
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+//  start new apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
+// change to false since it's graphql
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +23,23 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// if accessed on the server, send all calls there
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+// new instance of Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+}
+
+// start apollo server
+startApolloServer(typeDefs, resolvers);
